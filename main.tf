55ddb674e3183e522b5538b7cc0316d4f9d6bf44
Siby-John-J <romysiby@gmail.com>
2026-08-18 17:51:45 +0530

    simple ec2 for test

--- main.tf
+++ main.tf
@@ -2,51 +2,12 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_s3_bucket" "nextjs_site" {
-  bucket = "siby-nextjs-site-2026" # pick your own unique suffix
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0c55b159cbfafe1f0" # Update with latest Amazon Linux AMI
+  instance_type = "t2.micro"
+  key_name      = "your-key-pair-name"
 
   tags = {
-    Name    = "siby-nextjs-site"
-    Purpose = "learning-nextjs-hosting"
+    Name = "MyFirstInstance"
   }
-}
-
-resource "aws_s3_bucket_public_access_block" "nextjs_access" {
-  bucket = aws_s3_bucket.nextjs_site.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_website_configuration" "nextjs_website" {
-  bucket = aws_s3_bucket.nextjs_site.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "404.html"
-  }
-}
-
-resource "aws_s3_bucket_policy" "nextjs_public_read" {
-  bucket = aws_s3_bucket.nextjs_site.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.nextjs_site.arn}/*"
-      }
-    ]
-  })
-
-  depends_on = [aws_s3_bucket_public_access_block.nextjs_access]
 }
