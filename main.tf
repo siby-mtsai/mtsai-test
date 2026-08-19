e311597759b43c510f6a45ddb150e42a25aa1e55
Siby-John-J <romysiby@gmail.com>
2026-08-19 11:29:13 +0530

    added submets in the vpc.

--- main.tf
+++ main.tf
@@ -9,3 +9,23 @@ resource "aws_vpc" "mtsai_vpc" {
     Name = "mtsai-vpc"
   }
 }
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.mtsai_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.mtsai_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "private-subnet-2"
+  }
+}
