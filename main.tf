0dc1facf44e25844d6ac16d80d7dfb2b45cacf77
Siby-John-J <romysiby@gmail.com>
2026-08-19 16:34:59 +0530

    added baston..

--- main.tf
+++ main.tf
@@ -37,6 +37,49 @@ resource "aws_subnet" "private_subnet_2" {
 }
 
 # ---------------------------------------------------
+# PUBLIC SUBNET (for bastion)
+# ---------------------------------------------------
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.mtsai_vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# ---------------------------------------------------
+# INTERNET GATEWAY + ROUTING (for public subnet)
+# ---------------------------------------------------
+resource "aws_internet_gateway" "mtsai_igw" {
+  vpc_id = aws_vpc.mtsai_vpc.id
+
+  tags = {
+    Name = "mtsai-igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.mtsai_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.mtsai_igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_rt_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# ---------------------------------------------------
 # RDS SUBNET GROUP
 # ---------------------------------------------------
 resource "aws_db_subnet_group" "mtsai_db_subnets" {
@@ -62,6 +105,15 @@ resource "aws_security_group" "cms_sg" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  # NEW: allow SSH from the bastion specifically
+  ingress {
+    description     = "SSH from bastion"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
   }
 
   egress {
@@ -101,6 +153,32 @@ resource "aws_security_group" "rds_sg" {
   }
 }
 
+# NEW: bastion security group
+resource "aws_security_group" "bastion_sg" {
+  name        = "mtsai-bastion-sg"
+  description = "Allow SSH from internet to bastion"
+  vpc_id      = aws_vpc.mtsai_vpc.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "mtsai-bastion-sg"
+  }
+}
+
 # ---------------------------------------------------
 # EC2 - CMS INSTANCE
 # ---------------------------------------------------
@@ -113,6 +191,21 @@ resource "aws_instance" "mtsai_cms" {
 
   tags = {
     Name = "mtsai-cms"
+  }
+}
+
+# ---------------------------------------------------
+# EC2 - BASTION INSTANCE (NEW)
+# ---------------------------------------------------
+resource "aws_instance" "bastion" {
+  ami                    = "ami-0ac7b260cf76d8865"
+  instance_type          = "t2.micro"
+  key_name               = "siby-ec2-key-pair"
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+
+  tags = {
+    Name = "mtsai-bastion"
   }
 }
 
@@ -142,4 +235,8 @@ resource "aws_db_instance" "mtsai_db" {
 # ---------------------------------------------------
 output "db_endpoint" {
   value = aws_db_instance.mtsai_db.endpoint
+}
+
+output "bastion_public_ip" {
+  value = aws_instance.bastion.public_ip
 }
