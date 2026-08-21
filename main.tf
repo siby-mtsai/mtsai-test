3b6c7502c280cbbdfcc3d4e592b12129ddb0f2e1
Siby-John-J <romysiby@gmail.com>
2026-08-21 15:08:53 +0530

    updaed tfstate.

--- main.tf
+++ main.tf
@@ -262,7 +262,7 @@ resource "aws_instance" "bastion" {
   ami                    = "ami-0ac7b260cf76d8865"
   instance_type          = "t2.micro"
   key_name               = "siby-ec2-key-pair"
-  subnet_id              = aws_subnet.public_subnet.id
+  subnet_id              = aws_subnet.private_subnet_1.id
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
 
   timeouts {
